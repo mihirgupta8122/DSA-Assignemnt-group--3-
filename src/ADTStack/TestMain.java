b21daad83f2bc377426fb2772f00d641223a20d1
Saihaj <saihajmann07@gmail.com>
2026-03-29 16:28:22 -0600

    more tests

--- src/ADTStack/TestMain.java
+++ src/ADTStack/TestMain.java
@@ -12,9 +12,23 @@ public class TestMain {
         int res1 = calculator.evaluatePostfixExpression(exp1);
         System.out.println("Test 1: " + exp1 + " = " + res1);
 
+        testCalculater("2 3 5 + +", 10);
+        testCalculater("9 3 /", 3);
+        testCalculater("4 3 *", 12);
+        testCalculater("16 12 -", 4);
+        testCalculater("6 2 *", 12);
+
         System.out.println("\n--- All Tests Completed Successfully ---");
     }
 
+    public static void testCalculater(String expression, int expectedResult) {
+        PostfixCalculator calculator = new PostfixCalculator();
+        int res1 = calculator.evaluatePostfixExpression(expression);
+        System.out.println("Test 1: " + expression + " = " + res1 + " = " + expectedResult);
+        if (res1 == expectedResult) {
+            System.out.print(" Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
 }
-
-   
